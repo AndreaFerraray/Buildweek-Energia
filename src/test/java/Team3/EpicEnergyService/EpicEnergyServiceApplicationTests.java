7fb422595120a8d2dd9c11a41e69ba3a9a885bad
Simone <simone.dangelo636@gmail.com>
2023-11-20 11:54:25 +0100

    inizio progetto

---
+++ src/test/java/Team3/EpicEnergyService/EpicEnergyServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package Team3.EpicEnergyService;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EpicEnergyServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
