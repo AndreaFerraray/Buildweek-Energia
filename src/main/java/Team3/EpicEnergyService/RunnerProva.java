dc02ad7060974db6386e37e70c87517eeaa3add1
Simone <simone.dangelo636@gmail.com>
2023-11-20 16:10:16 +0100

    leggi file e controllo inserimento

---
+++ src/main/java/Team3/EpicEnergyService/RunnerProva.java
@@ -0,0 +1,24 @@
+package Team3.EpicEnergyService;
+
+import Team3.EpicEnergyService.services.AddressesService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Component;
+
+import java.io.File;
+import java.util.Arrays;
+import java.util.List;
+
+@Component
+public class RunnerProva implements CommandLineRunner {
+    @Autowired
+    private AddressesService addressesService;
+
+    @Override
+    public void run(String... args) throws Exception {
+        File provinceFile = new File("/Users/Simo/Documents/Epicode/BackEnd/Buildweek-Energia/file/province-italiane.csv");
+        List<String[]> r = addressesService.readFile(provinceFile);
+        r.forEach(x -> System.out.println(Arrays.toString(x)));
+        addressesService.saveAddress();
+    }
+}
---
+++ src/main/java/Team3/EpicEnergyService/entities/AddressesDTO.java
@@ -0,0 +1,4 @@
+package Team3.EpicEnergyService.entities;
+
+public record AddressesDTO(String street, int zipCode, int civicNumber, String city, String province) {
+}
